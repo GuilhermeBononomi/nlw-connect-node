6ddd8e9f26f63d13de43c82414acc240fc508d15
Guilherme Bononomi <gui.bononomi@gmail.com>
2025-02-19 14:24:02 -0300

    feat: create the first route and setup the essential files

---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+	validatorCompiler,
+	serializerCompiler,
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
+import { env } from "./env";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+	origin: "http://localhost:3000",
+});
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "NLW connect",
+			version: "0.0.1",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+	routePrefix: "/docs",
+});
+
+app.register(subscribeToEventRoute);
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.listen({ port: env.PORT }).then(() => {
+	console.log("HTTP server is running!");
+});
